9dc56a698444610a9719309c37f835901e8957cd
Kleiton Lucas <kleitonlucs@gmail.com>
2022-02-24 10:47:57 -0300

    Finalizando a página de confirmação de cadastro.

--- src/pages/Finish/index.tsx
+++ src/pages/Finish/index.tsx
@@ -1,11 +1,29 @@
 import * as C from "./style";
 import { Theme } from "../../components/Theme";
+import { useNavigate } from "react-router-dom";
+import { FormActions, useForm } from "../../contexts/FormContext";
+import { useEffect } from "react";
 
 export const Finish = () => {
+    const navigate = useNavigate();
+    const {state, dispatch} = useForm();
+
+    useEffect(()=>{
+        if(state.name === ''){
+            navigate("/");
+        } else {
+            dispatch({
+                type: FormActions.setCurrentsStep,
+                payload: 5
+            });
+        }
+    },[]);
+
     return (
         <Theme>
             <C.Container>
-                <h1>Cadastro Finalizado!</h1>
+                <h2>Cadastro Finalizado!</h2>
+                <h3>{state.name}, obrigado por se cadastrar em nossa plataforma.</h3>
                 <p>
                     Seu cadastro foi realizado com sucesso. Aguarde, em breve você estará recebendo e-mails 
                     de assuntos do seu interesse.
--- src/pages/Finish/style.ts
+++ src/pages/Finish/style.ts
@@ -1,10 +1,16 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    h1{
+    h2{
         font-size: 32px;
         text-align: center;
         padding-bottom: 20px;
+    }
+    h3{
+        font: 26px;
+        text-align: center;
+        padding-bottom: 40px;
+        color: #B8B8D4;
     }
     p{
         text-align: center;
